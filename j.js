5f87e09ebfc104a2672cb9c251ee7776a9f67840
ajcudd <ajcuddeback@gmail.com>
2021-01-19 18:23:03 -0500

    refactored viewDep function

--- j.js
+++ j.js
@@ -2,38 +2,116 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
 
 
-// const viewRoles = () => {
+// const upEmp = () => {
+//     console.log('in')
+//     // Select all roles from table for future ref
 //     connection.query(
-//         `SELECT roles.id, roles.title, roles.salary, department.name
-//             FROM roles
-//             LEFT JOIN department
-//             ON roles.department_id = department.id `,
+//         `SELECT * FROM roles`,
 //         function (err, results, fields) {
 //             if (err) {
 //                 console.log(err.message);
 //                 return;
 //             }
 
-//             console.table(results);
-//             promptUser();
-//         }
-//     )
-// }
+//             // Create empty array for storing info
+//             let roleArr = [];
 
-async function viewRoles() {
+//             // for each item in the results array, push the name of the roles to the roles array
+//             results.forEach(item => {
+//                 roleArr.push(item.title)
+//             })
+//             connection.query(
+//                 `SELECT first_name, last_name FROM employee`,
+//                 function (err, results, fields) {
+//                     if (err) {
+//                         console.log(err.message);
+//                     }
+
+//                     let nameArr = [];
+//                     results.forEach(item => {
+//                         nameArr.push(item.first_name);
+//                         nameArr.push(item.last_name);
+//                     })
+//                     let combinedNameArr = [];
+//                     for (let i = 0; i < nameArr.length; i += 2) {
+//                         if (!nameArr[i + 1])
+//                             break
+//                         combinedNameArr.push(`${nameArr[i]} ${nameArr[i + 1]}`)
+//                     }
+//                     inquirer
+//                         .prompt([
+//                             {
+//                                 type: 'list',
+//                                 name: 'name_select',
+//                                 message: 'Please select an employee you would like to update',
+//                                 choices: combinedNameArr
+//                             },
+//                             {
+//                                 type: 'list',
+//                                 name: 'role_select',
+//                                 message: 'Please select a role you would like your employee to change to:',
+//                                 choices: roleArr
+//                             }
+//                         ])
+//                         .then((data) => {
+//                             let role_id;
+//                             for (let i = 0; i < roleArr.length; i++) {
+//                                 if (data.role_select === roleArr[i]) {
+//                                     role_id = i + 1;
+//                                 }
+//                             };
+//                             let selectedNameArr = data.name_select.split(" ");
+//                             let last_name = selectedNameArr.pop();
+//                             let first_name = selectedNameArr[0];
+
+//                             connection.query(
+//                                 `UPDATE employee 
+//                                     SET role_id = ?
+//                                     WHERE first_name = ? AND last_name = ?`,
+//                                 [role_id, first_name, last_name],
+//                                 function (err, results, fields) {
+//                                     if (err) {
+//                                         console.log(err.message);
+//                                         return;
+//                                     }
+//                                     console.log('Employee updated!');
+//                                     promptUser();
+//                                 }
+//                             );
+//                         });
+//                 }
+//             );
+
+//         }
+//     );
+// };
+
+// async function getRoleTitle() {
+
+// }
+// View department
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'employees'
+});
+
+async function viewDep() {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'password',
         database: 'employees'
     });
-    const [rows, fields] = await connection.execute(`SELECT roles.id, roles.title, roles.salary, department.name
-    FROM roles
-    LEFT JOIN department
-    ON roles.department_id = department.id`);
+    const [rows, fields] = await connection.execute(`SELECT * FROM department`);
     await connection.end();
 
     console.table(rows);
     promptUser();
 }
-viewRoles()
+
+
+
+viewDep()
