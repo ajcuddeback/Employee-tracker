89f2fc88ef54e26af2769d748a04a3f5c8bfe5ae
ajcudd <ajcuddeback@gmail.com>
2021-01-22 15:20:35 -0500

    fixign issues

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,12 @@ DROP TABLE IF EXISTS manager;
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employee;
+CREATE TABLE manager (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
@@ -26,13 +32,7 @@ CREATE TABLE employee (
     manager_id INT,
     manager_confirm BOOLEAN,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES roles(id) ON DELETE
     SET NULL
-);
-CREATE TABLE manager (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    PRIMARY KEY (id)
 );
